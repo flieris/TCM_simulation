e5a77d6da546e3da664da6d11541f9e0430ec314
Sebastian Lenkiewicz <lenniuch@hotmail.com>
2017-09-06 21:25:27 +0200

    new channel version -- to test

--- TCM_simulation/channel.cpp
+++ TCM_simulation/channel.cpp
@@ -1,6 +1,7 @@
 #include "channel.h"
 #include <cmath>
 #include <iostream>
+using namespace std;
 Channel::Channel(double snr) {
    variance = pow(10.0, (-snr / 10.0));
    noise_generator = new Generator(420);
@@ -11,6 +12,9 @@ Channel::~Channel() {
 }
 
 std::complex<double> Channel::noisify(std::complex<double> input) {
+   double noise_real = noise_generator->Random() * variance * (1 / sqrt(variance));
+   double noise_imag = noise_generator->Random() * variance * (1 / sqrt(variance));
+   cout << "Noise values: " << endl << "real: " << noise_real << " imag: " << noise_imag << endl;
    double real_value = input.real() + noise_generator->Random(- variance, variance);
    double imag_value = input.imag() + noise_generator->Random(- variance, variance);
    return std::complex<double>(real_value, imag_value);
--- TCM_simulation/main.cpp
+++ TCM_simulation/main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 
 int main() {
+   cout << pow(10.0, (-15 / 10.0)) << endl;
    Generator *source_ = new Generator(123);
    Coder *coder_ = new Coder();
    Modulator *modulator_ = new Modulator();
@@ -22,7 +23,7 @@ int main() {
    std::vector<int> entry_symbol, exit_symbol;
    
    cout << "Conducting test" << endl;
-   for (int i = 0; i < 500; ++i) {
+   for (int i = 0; i < 5; ++i) {
       symbol = static_cast<int>(source_->Random(0, 4));
       entry_symbol.push_back(symbol);
       cout << "Symbol = " << symbol << endl;
@@ -30,7 +31,7 @@ int main() {
       cout << "Encoded symbol = "<< encoded_symbol << endl;
       modulator_->modulate(encoded_symbol);
       modulated = modulator_->getOutputData();
-      //noisy = channel->noisify(modulated);
+      noisy = channel->noisify(modulated);
       //cout << "Data after noisy channel: " << noisy << endl;
       cout << "Symbol after 8PSK modulation = " << modulator_->getOutputData() << endl;
       decoder->decode(modulated);
