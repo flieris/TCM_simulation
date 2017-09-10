4b2d802ee41dbbb5350c427313d907d44a585ea7
Sebastian Lenkiewicz <lenniuch@hotmail.com>
2017-09-10 15:21:09 +0200

    Changed coder structure

--- TCM_simulation/coder.cpp
+++ TCM_simulation/coder.cpp
@@ -14,8 +14,7 @@ Coder::~Coder() {
 unsigned int Coder::encode(unsigned int data) {
    unsigned int c0 = data >> 1;
    unsigned int c1 = (state & 1) ^ (data & 1);
-   unsigned int c2 = c1 ^ (state >> 1);
-   
+   unsigned int c2 = (state >> 1);
    unsigned int c = (c0 << 2) | (c1 << 1) | c2;
    state = (state >> 1) | ((data & 1) << 1);
    //cout << state << endl;
